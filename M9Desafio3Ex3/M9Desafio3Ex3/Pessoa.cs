ad7a5465bb585f98548d7238c6ae8f422874af69
devthony <104657459+anthonyfuzinato@users.noreply.github.com>
2024-10-10 09:17:09 +0100

    Add files via upload

---
+++ M9Desafio3Ex3/M9Desafio3Ex3/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9Desafio3Ex3
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string cidade;
+
+        public Pessoa(string nomePessoa, int idadePessoa, string cidadePessoa)
+        {
+            nome = nomePessoa;
+            idade = idadePessoa;
+            cidade = cidadePessoa;
+        }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Nome: {nome}, Idade: {idade}, Cidade: {cidade}");
+        }
+    }
+}
---
+++ M9Desafio3Ex3/M9Desafio3Ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9Desafio3Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Alice", 28, "São Paulo");
+            Pessoa pessoa2 = new Pessoa("Bruno", 35, "Rio de Janeiro");
+            Pessoa pessoa3 = new Pessoa("Carla", 22, "Viseu");
+
+            pessoa1.ExibirDetalhes();
+            pessoa2.ExibirDetalhes();
+            pessoa3.ExibirDetalhes();
+        }
+    }
+}
